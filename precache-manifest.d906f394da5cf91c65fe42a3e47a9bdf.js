4a3d2795a43204a64297d13ed0eaa5ad481e109e
satya-1210 <satya.1210@gmail.com>
2020-09-14 12:03:09 +0530

    Updates

---
+++ precache-manifest.d906f394da5cf91c65fe42a3e47a9bdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6bf75e2722b0b9e77039adecf633fbb",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "df151843932edf551afe",
+    "url": "/to-do-list/static/css/main.eae54c5a.chunk.css"
+  },
+  {
+    "revision": "54e4585e6eac38b267be",
+    "url": "/to-do-list/static/js/2.43af7817.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list/static/js/2.43af7817.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df151843932edf551afe",
+    "url": "/to-do-list/static/js/main.055c258c.chunk.js"
+  },
+  {
+    "revision": "f8d4f1e2f9cc73a5532a",
+    "url": "/to-do-list/static/js/runtime-main.9e5907d5.js"
+  }
+]);
